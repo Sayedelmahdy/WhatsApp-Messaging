cd9acbcb5c9119fd34c963fdaab00e717355d41a
Sayedelmahdy <sayedahmed01093307397@gmail.com>
2024-04-30 23:52:33 +0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -5,9 +5,9 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Your API Documentation',
+            title: 'WhatsApp Message OTP',
             version: '1.0.0',
-            description: 'API documentation generated with Swagger',
+            description: '',
         },
     },
     apis: ['./routers/*.js'], // Path to the files containing the JSDoc comments
